def8f9a6eb87e3fd64cb42661f6c3e6e8f83331c
chaitany <chaitanyjanmale@gmail.com>
2021-08-13 09:08:07 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+   function validate(e){
+           let namePipex=document.getElementById("namePipe").value;//1st input field
+           let projectIdx=document.getElementById("projectId").value;//2nd input field
+           let bucketNamex=document.getElementById("bucketName").value;//3rd input field
+           let fileNamex=document.getElementById("fileName").value;//4th input field
+           let gcsCredx=document.getElementById("gcsCred").value;//5th input field
+           let timeMinx=document.getElementById("timeMin").value;//6th input field
+           let pattern=/^[^\+_\-\W][\w]{4,4}$/g 
+  
+
+        if((pattern.test(namePipex)||pattern.test(projectIdx)||pattern.test(bucketNamex)||pattern.test(fileNamex)||pattern.test(gcsCredx)||pattern.test(timeMinx))==false)
+        {
+             alert("no empty field and dont put special char,+ -_ any special char and also input should be min 5 of length ")
+                    return false
+        }      
+     else{                                        
+                    return true;
+         }
+ 
+}
+
+document.getElementById("formSubmit").onsubmit=(e)=>{ 
+
+
+    e.preventDefault()                                                    
+    if(validate(e)){ 
+                  
+          
+        alert("submitteeddd!!!!!")
+        
+     
+    } 
+
+document.getElementById("formSubmit").reset()
+
+
+
+
+
+
+} 
+
+
