8715350afb0e63c489ce4df74a2afed5823bba82
chaitany <chaitanyjanmale@gmail.com>
2021-08-19 12:07:34 +0530

    some error of validation resolve

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
-   function validate(e){
+   function validate(){
            let namePipex=document.getElementById("namePipe").value;//1st input field
-           let projectIdx=document.getElementById("projectId").value;//2nd input field
+            let projectIdx=document.getElementById("projectId").value;//2nd input field
            let bucketNamex=document.getElementById("bucketName").value;//3rd input field
            let fileNamex=document.getElementById("fileName").value;//4th input field
            let gcsCredx=document.getElementById("gcsCred").value;//5th input field
            let timeMinx=document.getElementById("timeMin").value;//6th input field
-           let pattern=/^[\w][^\+_\-\W]{4,4}$/g 
-  
-
-        if(!(pattern.test(namePipex)||pattern.test(projectIdx)||pattern.test(bucketNamex)||pattern.test(fileNamex)||pattern.test(gcsCredx)||pattern.test(timeMinx)))
+           let pattern=/^[a-zA-Z0-9]*[^\+_\-\s\W]$/ ; 
+          
+ 
+    /* if((((pattern.test(namePipex))||(pattern.test(projectIdx))||(pattern.test(bucketNamex))||(pattern.test(fileNamex))||(pattern.test(gcsCredx))||(pattern.test(timeMinx)))==false)&&(namePipex.length||gcsCredx.length||bucketNamex.length||timeMinx.length||fileNamex.length||projectIdx.length)==0)
         {
-             alert("no empty field and dont put special char,+ -_ any special char and also input should be min 5 of length ")
-                    return false
-        }      
-     else{                                        
-                    return true;
-         }
+             alert("NO SPACES"+"=="+"MIN LENGTH 5"+"=="+"NO SPECIAL CHAR"+"=="+"NO START WITH +,-,_")
+                    return false;
+       
+        } */
+  
+        if(
+            ((
+            (pattern.test(namePipex))&&(pattern.test(projectIdx))&&(pattern.test(bucketNamex))&&(pattern.test(fileNamex))&&(pattern.test(gcsCredx))&&(pattern.test(timeMinx))
+            
+            )==true)
+            
+            &&
+            ((namePipex.length)&&(gcsCredx.length)&&(bucketNamex.length)&&(timeMinx.length)&&(fileNamex.length)&&(projectIdx.length))
+
+            <=5
+            )
+    {
+       
+        return true
+    }else{ 
+        alert("NO SPACES"+"=="+"MIN LENGTH 5"+"=="+"NO SPECIAL CHAR"+"=="+"NO START WITH +,-,_")
+        return false
+       
+    }
+        
+            
+           
+    
  
 }
 
 document.getElementById("formSubmit").onsubmit=(e)=>{ 
 
 
-    e.preventDefault()                                                    
-    if(validate(e)){ 
+    e.preventDefault()//prevent subimisstion after click submit 
+                                                   
+    if(validate()){ 
                   
           
         alert("submitteeddd!!!!!")
